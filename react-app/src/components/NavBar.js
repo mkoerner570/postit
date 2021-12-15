5e5d4499325805dd4f46d639285edf742f8ab5c7
Max K <mkoerner570@gmail.com>
2021-12-15 17:38:01 -0500

    more component work started, testing to follow

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -1,36 +1,101 @@
-
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import * as sessionActions from '../store/session';
+import React,{useState, useEffect} from 'react';
+import { Link, NavLink, useParams } from 'react-router-dom';
+import { useSelector, useDispatch } from "react-redux";
 import LogoutButton from './auth/LogoutButton';
-
+ // test
 const NavBar = () => {
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
+  const dispatch = useDispatch()
+  const [user, setUser] = useState({});
+  const { userId }  = useParams();
+  const sessionUser = useSelector((state) => state.session.user);
+
+  const demoLogin = async () => {
+    setCredential('demo@aa.io');
+    setPassword('password');
+    return dispatch(
+      sessionActions.login('demo@aa.io', 'password')
+    );
+  }
+
+  useEffect(() => {
+    if (!userId) {
+      return;
+    }
+    (async () => {
+      const response = await fetch(`/api/users/${userId}`);
+      const user = await response.json();
+      setUser(user);
+    })();
+  }, [userId]);
+
+  let demo;
+  if(!sessionUser){
+    demo = <button className='demo' onClick={demoLogin}>Demo Login</button>
+  }
+
+  let signin;
+  if(!sessionUser){
+    signin=<NavLink to='/login' exact={true} activeClassName='active' className='link-signin'>
+    Sign In
+  </NavLink>
+  }
+
+  let create;
+  if(!sessionUser){
+    create = <NavLink to='/sign-up' exact={true} activeClassName='active' className='links'>
+    Create Account
+  </NavLink>
+  }
+
+  let logOut;
+  if(sessionUser){
+    logOut = <LogoutButton/>
+  }
+
+  let profileButton;
+  if(sessionUser){
+    profileButton =  <NavLink to={`/users/${sessionUser.id}`} exact={true} activeClassName='active' className='links'>
+    Profile
+  </NavLink>
+  }
+  let uploadButton;
+  if(sessionUser){
+    uploadButton =  <NavLink to={'/upload'} activeClassName='active' className='links'>
+    Upload
+  </NavLink>
+  }
+
   return (
     <nav>
+      <div className="nav">
+      <div className="the-logo">
+        <Link to="/" className="homie">
+          <img src='https://res.cloudinary.com/dzjkwepju/image/upload/v1635992335/Styckr/1-Angry-cloud_xxckko.png' alt='logo'/>
+           AngryCloud
+        </Link>
+      </div>
       <ul>
         <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
-            Home
-          </NavLink>
+          {demo}
         </li>
         <li>
-          <NavLink to='/login' exact={true} activeClassName='active'>
-            Login
-          </NavLink>
+          {signin}
         </li>
         <li>
-          <NavLink to='/sign-up' exact={true} activeClassName='active'>
-            Sign Up
-          </NavLink>
+          {create}
+          {uploadButton}
         </li>
         <li>
-          <NavLink to='/users' exact={true} activeClassName='active'>
-            Users
-          </NavLink>
+          {profileButton}
         </li>
         <li>
-          <LogoutButton />
+          {logOut}
         </li>
       </ul>
+      </div>
     </nav>
   );
 }
---
+++ react-app/src/components/commentform.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { useHistory } from 'react-router-dom';
+import { useSelector } from "react-redux";
+import { AddAComment, GetAllComments } from "../store/Comments";
+import { EditDelete2 } from "./edit-delete";
+
+
+function CommentForm({post_id}) {
+    const dispatch = useDispatch()
+    const [content, setContent] = useState("")
+    const history = useHistory()
+    const [postId,setPostId] = useState(post_id)
+
+    useEffect(()=>{
+        dispatch(GetAllComments(postId))
+    },[dispatch])
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        let payload = {content, postId}
+        dispatch(AddAComment(payload))
+
+        history.push(`/post/${post_Id}`)
+    }
+
+    return (
+        <div>
+        <form className="CommentForm" onSubmit={handleSubmit}>
+            <label className="noteForms">
+            <textarea
+                    id='comment'
+                    type="textarea"
+                    value={content}
+                    onChange={(e) => setContent(e.target.value)}
+                    placeholder="Your Comment here"
+            />
+            </label>
+            <button id="submit" type="submit">Submit</button>
+        </form>
+
+        <div>
+        {
+                        props.comments.map(comment => {
+                            return <div className="comments">
+                                <div>
+                                    <div className="plusOne">
+                                        <i class="fa fa-angle-up"></i>
+                                    </div>
+                                    <div className="minusOne">
+                                        <i class="fa fa-angle-down"></i>
+                                    </div>
+                                </div>
+
+                                <div>
+                                    <div>{comment.body}</div>
+                                    <div className="comment-info">Votes{comment.votes}</div>
+                                    <div className="comment-info">By: {comment.username}</div>
+                                </div>
+                            </div>
+                        })
+                    }
+        </div>
+        </div>
+    )
+}
+
+
+export default CommentForm;
